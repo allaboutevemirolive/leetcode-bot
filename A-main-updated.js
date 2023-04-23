f4cbcbb3b56ac8671ffffbb52edc7bdf3394aadb
allaboutevemirolive <xkmxlfirdxus@gmail.com>
2023-04-23 03:18:19 -0400

    new_line

--- A-main-updated.js
+++ A-main-updated.js
@@ -5,9 +5,9 @@ const fs = require('fs');
     const browser = await chromium.launch();
     const page = await browser.newPage();
 
-    const text_file = 'LeetCode-Hard-Page-1.txt'
+    const text_file = 'LeetCode-Hard-Page-8.txt'
 
-    const target_link = 'https://leetcode.com/problemset/all/?difficulty=HARD&page=1';
+    const target_link = 'https://leetcode.com/problemset/all/?difficulty=HARD&page=8';
 
     const target_language_class = '.language-rust, .language-python, .language-java, .language-cpp';
 
@@ -16,8 +16,10 @@ const fs = require('fs');
     const mapping = {};
 
     for (const match of matches) {
+
         await page.goto(target_link);
-        await page.waitForTimeout(5000);
+
+        await page.waitForTimeout(6000);
 
         console.log('match: ' + match);
 
@@ -43,29 +45,37 @@ const fs = require('fs');
 
         try {
             const targetUrl = match + "solutions/?orderBy=newest_to_oldest";;
+
             await page.goto(targetUrl);
 
             await page.waitForTimeout(3000);
 
-            const searchInput = await page.$('input[type="text"][placeholder="Search"]');
+            const searchInput = await page.$('input[type="text"][placeholder="Search..."]');
+
             await searchInput.fill('rust');
 
             await page.waitForTimeout(5000);
 
         } catch (err) {
             console.log(err);
+
             console.log('Page not found. Not premium user');
+
             continue;
         }
 
         try {
-            const button_sort = await page.$('#headlessui-menu-button-\\:Rqaa9j9l5t6\\:');
+            const button_sort = await page.$('#headlessui-menu-button-\\:Rmaa9j9l5t6\\:');
+
             await button_sort.click();
 
             const button_recent = await page.$('div.truncate:has-text("Most Recent")');
+
             await button_recent.click();
+
         } catch (err) {
             console.log(err);
+
             console.log('No sort button');
         }
 
@@ -83,12 +93,14 @@ const fs = require('fs');
         for (const link of links) {
 
             await page.goto(link);
+
             await page.waitForTimeout(3000);
 
             const pathSegments = link.split('/').filter(str => str !== "");
 
             // We need to change "-" to "_" to follow the naming convention
             const problemName = pathSegments[pathSegments.indexOf("problems") + 1];
+
             const updatedProblemName = problemName.replace(/-/g, '_');
 
             const solutionId = pathSegments[pathSegments.indexOf("solutions") + 1];
@@ -96,19 +108,24 @@ const fs = require('fs');
             const reconstructedString = `${updatedProblemName}${solutionId}`;
 
             const rustButton = await page.$('div.relative.cursor-pointer.px-3.py-3.text-label-4.dark\\:text-dark-label-4.hover\\:text-label-1.dark\\:hover\\:text-dark-label-1.GMIHh:has-text("Rust")');
-            
+
             if (rustButton) {
                 await rustButton.click();
+
                 console.log('Clicked Rust hidden button / there is more than one language');
             } else {
+
                 console.log('Rust button is visible or there is only one language');
             }
 
             try {
 
                 const dataElement = await page.waitForSelector(target_language_class);
+
                 await page.waitForTimeout(3000);
+
                 const dataText = await dataElement.innerText();
+
                 await page.waitForTimeout(3000);
 
                 fs.mkdir(folderName, (err) => {
@@ -129,6 +146,6 @@ const fs = require('fs');
             }
         }
     }
-    
+
     await new Promise(() => { });
 })();
